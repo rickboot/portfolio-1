12db76cf636d947d1210c6277e85851b1ee6adc7
Rick Allen <rick@Ricks-MacBook-Air.local>
2025-06-07 01:19:30 -0700

    fix: vercel ignores vercel.json for nextjs apps

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+    async redirects() {
+        return [
+        {
+            source: '/:path*',
+            has: [
+            {
+                type: 'host',
+                value: 'rickallen.rocks',
+            },
+            ],
+            destination: 'https://rickallen.dev/:path*',
+            permanent: true,
+        },
+        {
+            source: '/:path*',
+            has: [
+            {
+                type: 'host',
+                value: 'www.rickallen.rocks',
+            },
+            ],
+            destination: 'https://rickallen.dev/:path*',
+            permanent: true,
+        },
+        ];
+    },
+};
 
 export default config;
